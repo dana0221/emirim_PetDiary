d9e23808317046c6bf5ef008befb4d41f9ec347a
dana8 <w2008@e-mirim.hs.kr>
2021-10-21 18:41:02 +0900

    Edit Pet Diary add Photo, Gallery Commit

---
+++ app/src/main/java/com/example/emirim_petdiary/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.emirim_petdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        linearLayout = findViewById(R.id.liner_main);
+
+        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+                return true;
+            }
+        });
+    }
+}
